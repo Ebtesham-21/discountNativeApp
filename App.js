94b4e9c7ca2715697b2e0200c4a4944e02c38a09
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-01-28 13:40:01 +0600

    build issue

--- App.js
+++ App.js
@@ -1,30 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View,SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
 
 
 export default function App() {
+  const [amount, setAmount] = useState(0);
+  const [discount, setDiscount] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const calculateDiscount = () => {
+    const numAmount = parseFloat(amount);
+    const numDiscount = parseFloat(discount);
+
+    if(isNaN(numAmount) || isNaN(numDiscount)){
+      setResult(0);
+      return;
+    }
+
+    if(numDiscount === 0) {
+      setResult(0);
+      return;
+    }
+
+    const discountAmount = numAmount * (numDiscount / 100);
+    setResult(discountAmount)
+  };
+  
+ 
   return (
     <SafeAreaView style={styles.container}>
     <View style={{ alignItems:'center', marginTop:40}}>
       <Text style={{fontSize: 22}}>Discount Calculator</Text>
       
     </View>
-    <View style={{marginHorizontal: 40, marginTop: 40, marginBottom: 20}}>
+    <View style={{marginHorizontal: 40, marginTop: 40, marginBottom: 10}}>
       <Text style={{marginBottom: 10}}>Amount</Text>
       <TextInput 
         style={styles.input}
-          
+        keyboardType = 'numeric'
+        onChangeText={setAmount}
+        value={amount}
       />
     </View>
-    <View style={{marginHorizontal: 40}}>
+    <View style={{marginHorizontal: 40, marginTop: 40, marginBottom: 20}}>
       <Text style={{marginBottom: 10}}>Discount(%)</Text>
       <TextInput 
         style={styles.input}
+        onChangeText={setDiscount}
+        value={discount}
           
       />
     </View>
     <View style={{alignItems: 'center'}}>
-       <TouchableOpacity style={ styles.btn}>
+       <TouchableOpacity style={ styles.btn}  onPress = {calculateDiscount}>
+         
           <Text style={{color:'black'}}>
             Calculate
           </Text>
@@ -32,9 +61,14 @@ export default function App() {
     </View>
 
     <Text
-    style={styles.result}
+    style={[styles.result, {color: result ? 'green' : 'black'} ]}
     >
       The Amount After Discount is:
+    </Text>
+    <Text
+    style={[styles.result, {color: result  ? 'green' : 'black'} ]}
+    >
+      {parseFloat(amount) - result}
     </Text>
     </SafeAreaView>
   );
@@ -48,10 +82,12 @@ const styles = StyleSheet.create({
   },
   input: {
            backgroundColor: 'white',
-           padddingVertical: 20, 
+           padddingVertical: 30, 
            borderRadius: 10,
            borderColor: 'black',
            borderWidth: 1,
+           fontSize: 20,
+           textAlign: 'center',
   },
   btn: {
     backgroundColor: 'lightgreen',
@@ -63,7 +99,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   result: {
-      color: 'green',
+      color: 'black',
       textAlign: 'center',
       fontSize: 22,
       marginTop: 20,
